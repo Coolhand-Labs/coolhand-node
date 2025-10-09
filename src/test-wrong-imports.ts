63563e120404729a81e74228f9c14f0fc55312a3
Michael Carroll <217594+mikecarroll@users.noreply.github.com>
2025-10-09 15:48:24 -0400

    update docs & use 0.1.0-rc1 as version

--- src/test-wrong-imports.ts
+++
@@ -1,5 +0,0 @@
-// Temporary test file to verify ESLint rule
-import { BaseService } from './services/BaseService';
-import { getCollectorString } from './utils/collector';
-
-export const test = true;
