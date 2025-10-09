d32f65a1cff9e512e7f62c424ff3cf6c0624b0e8
Michael Carroll <217594+mikecarroll@users.noreply.github.com>
2025-10-09 12:45:11 -0400

    Add collector data (#10)
    * add collector
    * add version constants
    * DRY services

---
+++ src/version.ts
@@ -0,0 +1,12 @@
+/**
+ * Single source of truth for package version
+ * This file is used by both the build process and runtime code
+ */
+
+export const PACKAGE_VERSION = '0.1.0';
+export const PACKAGE_NAME = 'coolhand-node';
+
+/**
+ * Full package identifier
+ */
+export const PACKAGE_IDENTIFIER = `${PACKAGE_NAME}-${PACKAGE_VERSION}`;
