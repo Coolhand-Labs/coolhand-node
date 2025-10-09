b499694639768d833d4951ff3aedbc8ee1334736
Michael Carroll <michael@carroll.io>
2025-10-09 12:32:35 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,5 @@ export * from './types.js';
 export * from './services/index.js';
 export * from './global-monitor.js';
 export * from './utils/collector.js';
+export * from './version.js';
 export default Coolhand;
