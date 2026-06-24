d49fb247b725a653cea0e25089c83a8f07b94393
Michael Carroll <michael@carroll.io>
2026-06-24 15:58:01 -0400

    Update version.ts

--- src/version.ts
+++ src/version.ts
@@ -3,7 +3,7 @@
  * Run `npm run sync-version` or `npm run build` to regenerate.
  */
 
-export const PACKAGE_VERSION = '0.7.0';
+export const PACKAGE_VERSION = '0.8.1';
 export const PACKAGE_NAME = 'coolhand-node';
 
 /**
